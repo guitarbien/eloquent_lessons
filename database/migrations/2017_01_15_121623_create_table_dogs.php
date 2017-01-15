ef03d2045c883e6fcb67d7742cdf29522bc2f27a
guitarbien <guitarbien@gmail.com>
2017-01-15 20:19:06 +0800

    add name column

--- database/migrations/2017_01_15_121623_create_table_dogs.php
+++ database/migrations/2017_01_15_121623_create_table_dogs.php
@@ -15,6 +15,7 @@ class CreateTableDogs extends Migration
     {
         Schema::create('dogs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
